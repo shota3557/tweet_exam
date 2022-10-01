437879b9e1448fe33978f71f5bf8607aa44fa7d8
shota kikuchi <shota3557@gmail.com>
2022-10-01 14:16:52 +0900

    リダイレクトできるようにした

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def new
+     @post = Post.new
+  end
+
+  def create
+    Post.create(content: params[:post][:content])
+    redirect_to new_post_path
+  end
+  
+  
+end

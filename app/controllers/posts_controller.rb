7415a1828aee6b1f116bdcfc92a08d4d889ab4c9
shota kikuchi <shota3557@gmail.com>
2022-10-01 19:06:18 +0900

    確認画面に戻る前にバリデーション発生させる機能搭載のため

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,28 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  
   def index
     @posts = Post.all
   end
 
   def new
-     @post = Post.new
+    @post = Post.new
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "ボヤキを消した"
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to posts_path, notice: 'ボヤき直した'
     else
@@ -21,16 +31,19 @@ class PostsController < ApplicationController
   end    
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def create
     @post = Post.new(post_params)
-      if @post.save
-        redirect_to new_post_path, notice: 'ボヤいた'
-     else
-      render :new
-     end  
+      if params[:back]
+        render :new
+      else
+        if @post.save
+          redirect_to new_post_path, notice: 'ボヤいた'
+        else
+          render :new
+        end 
+      end   
   end
   
   private
@@ -39,5 +52,7 @@ class PostsController < ApplicationController
     params.require(:post).permit(:content)
   end
   
-
+  def set_post
+    @post = Post.find(params[:id])
+  end  
 end

7a652f4cbeb2311be4e14b071fa55a2cd6c6c812
shota kikuchi <shota3557@gmail.com>
2022-10-01 16:03:58 +0900

    updateアクションを追加した

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,19 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: 'ボヤき直した'
+    else
+      render :edit
+    end
+  end    
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = Post.new(post_params)
       if @post.save
@@ -26,4 +39,5 @@ class PostsController < ApplicationController
     params.require(:post).permit(:content)
   end
   
+
 end

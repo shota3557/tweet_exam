aeff9aff524cd1a78c6ff7fe8b4a2c5f09a077a3
shota kikuchi <shota3557@gmail.com>
2022-10-01 14:22:45 +0900

    ストロングパラメータの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,9 +7,14 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:post][:content])
+    Post.create(post_params)
     redirect_to new_post_path
   end
   
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
   
 end
